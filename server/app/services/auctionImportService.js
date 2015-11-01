db2fc52b04a2cc4f1fe52da0479737980b397dee
Bmagic <baptiste.routhieau@gmail.com>
2015-11-01 14:08:52 +0100

    Optimize import

--- server/app/services/auctionImportService.js
+++ server/app/services/auctionImportService.js
@@ -3,6 +3,7 @@ var logger = process.require("api/logger.js").get("logger");
 var bnetAPI = process.require("api/bnet.js");
 var async = require("async");
 var guildUpdateModel = process.require("models/guildUpdateModel.js");
+var guildService = process.require("services/guildService.js");
 
 //Configuration
 var env = process.env.NODE_ENV || 'dev';
@@ -17,20 +18,26 @@ module.exports.importAuctions = function(){
                 logger.error(error.message)
                 return;
             }
-
             async.eachSeries(realms.realms,function(realm,callback){
                 bnetAPI.getAuctions(region, realm.name, function (error, auctions) {
-
                     async.eachSeries(auctions.auctions, function (auction, callback) {
-
                         bnetAPI.getCharacter(region, realm.name, auction.owner, function (error, character) {
                             if (error || !character.guild) {
                                 callback();
                                 return;
                             }
-                            guildUpdateModel.insertOrUpdate(region, character.realm, character.guild.name,0,function(error){
-                                logger.info("Insert guild  to update "+ region+"-"+character.realm+"-"+character.guild.name);
-                                callback();
+                            guildService.get(region, character.realm, character.guild.name,function(error,guild){
+                                if (error || !character.guild) {
+                                    callback();
+                                    return;
+                                }
+                                var timestampWeekAgo = new Date().getTime() - (7 * 24 * 3600 * 1000);
+                                if(guild == null || guild.bnet.updated < timestampWeekAgo ) {
+                                    guildUpdateModel.insertOrUpdate(region, character.realm, character.guild.name, 0, function (error) {
+                                        logger.info("Insert guild  to update " + region + "-" + character.realm + "-" + character.guild.name);
+                                        callback();
+                                    });
+                                }
                             });
                         });
                     },function(){
