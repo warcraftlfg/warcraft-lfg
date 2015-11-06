c79a490fb605f2c9ab4737c48184924dfacb1fbc
Baptiste <baptiste.routhieau@gmail.com>
2015-11-06 14:25:37 +0100

    Autocomplete optimisation

--- server/app/services/searchService.js
+++ server/app/services/searchService.js
@@ -1,17 +1,33 @@
 "use strict";
 var guildModel = process.require("models/guildModel.js");
 var characterModel = process.require("models/characterModel.js");
+var async = require("async");
+
 module.exports.search = function(search,callback){
 
-    var result={};
-    guildModel.search(search,function(error,guilds){
-        if(error)
-            return callback(error);
 
-        result.guilds = guilds;
-        characterModel.search(search,function(error,characters){
-            result.characters = characters;
-            callback(error,result);
-        });
+    async.parallel({
+        guilds : function(callback){
+            guildModel.search(search,function(error,guilds) {
+                if (error)
+                    return callback(error);
+                console.log("guildModel")
+                callback(null,guilds);
+            });
+        },
+        characters :function(callback){
+            characterModel.search(search,function(error,characters){
+                if (error)
+                    return callback(error);
+                console.log("characterModel")
+                callback(null,characters);
+            });
+        }
+    }, function(error,result){
+        console.log("tata"),
+        console.log(result);
+        callback(error,result)
     });
+
+
 };
