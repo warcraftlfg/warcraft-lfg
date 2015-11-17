07254de891fe88f7c11c352867bfa0141650b0b0
baptiste <baptiste@bmagic.fr>
2015-11-17 22:28:34 +0100

    Refactoring sockets

--- server/app/sockets/userSocket.js
+++ server/app/sockets/userSocket.js
@@ -1,9 +1,5 @@
 "use strict";
 
-/**
- * Provides function which listen socket.io and give user information.
- */
-
 //Modules dependencies
 var userService = process.require("services/userService.js");
 var logger = process.require("api/logger.js").get("logger");
@@ -11,7 +7,7 @@ var applicationStorage = process.require("api/applicationStorage.js");
 
 module.exports.connect = function(){
     var io = applicationStorage.getSocketIo();
-    //Listen for new user's connections
+
     io.on('connection', function(socket){
         if(!socket.request.user.logged_in) {
             logger.info("Anonymous user connected");
@@ -22,9 +18,6 @@ module.exports.connect = function(){
             //Send to user is information
             socket.emit('get:user',socket.request.user);
 
-            /**
-             * Return bnet guilds for current user
-             */
             socket.on('get:userGuilds', function(region) {
                 userService.getGuilds(region,socket.request.user.id, function (error,guilds) {
                     if (error)
@@ -33,9 +26,6 @@ module.exports.connect = function(){
                 });
             });
 
-            /**
-             * Return bnet characters for current user
-             */
             socket.on('get:userCharacters', function(region) {
                 userService.getCharacters(region,socket.request.user.id, function (error,characters) {
                     if (error)
