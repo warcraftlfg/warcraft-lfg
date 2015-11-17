6a50a9e917b00d9cdc9e3c0e52f9432a7cd516e6
baptiste <baptiste@bmagic.fr>
2015-11-17 21:34:31 +0100

    Refactoring userSocket.js

--- server/app/sockets/userSocket.js
+++ server/app/sockets/userSocket.js
@@ -1,8 +1,7 @@
-"use strict"
-
+"use strict";
 
 /**
- * Provides function which listen socket.io and give user informations.
+ * Provides function which listen socket.io and give user information.
  */
 
 //Modules dependencies
@@ -14,22 +13,22 @@ module.exports.connect = function(){
     var io = applicationStorage.getSocketIo();
     //Listen for new user's connections
     io.on('connection', function(socket){
-
         if(!socket.request.user.logged_in) {
             logger.info("Anonymous user connected");
         }
         else {
             logger.info( socket.request.user.battleTag + " connected");
 
+            //Send to user is information
+            socket.emit('get:user',socket.request.user);
+
             /**
              * Return bnet guilds for current user
              */
             socket.on('get:userGuilds', function(region) {
                 userService.getGuilds(region,socket.request.user.id, function (error,guilds) {
-                    if (error) {
-                        socket.emit("global:error", error.message);
-                        return;
-                    }
+                    if (error)
+                        return socket.emit("global:error", error.message);
                     socket.emit("get:userGuilds", guilds);
                 });
             });
@@ -38,21 +37,12 @@ module.exports.connect = function(){
              * Return bnet characters for current user
              */
             socket.on('get:userCharacters', function(region) {
-
                 userService.getCharacters(region,socket.request.user.id, function (error,characters) {
-                    if (error) {
-                        socket.emit("global:error", error.message);
-                        return;
-                    }
+                    if (error)
+                        return socket.emit("global:error", error.message);
                     socket.emit("get:userCharacters", characters);
-
                 });
             });
         }
-
-        //Send to user is informations
-        socket.emit('get:user',socket.request.user);
     });
-
-
 };
