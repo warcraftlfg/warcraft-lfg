9ea6f4b6275fa344f3e975733fd198ec8dc36949
Baptiste <baptiste.routhieau@gmail.com>
2015-11-17 12:04:20 +0100

    Bug on guild et charaterlist

--- server/app/models/characterModel.js
+++ server/app/models/characterModel.js
@@ -191,7 +191,7 @@ module.exports.getAds = function(number, filters, callback){
     var criteria ={"ad.updated":{$exists:true}};
     var filters = filters || {};
     if(filters.last){
-        criteria.updated={$lt:filters.last}
+        criteria["ad.updated"]={$lt:filters.last}
     }
     if(filters.lvlmax){
         criteria["bnet.level"] = {$gte:100};
