89b3265757341120666703fd22b8acb8dbd11d1d
baptiste <baptiste@bmagic.fr>
2015-11-21 09:11:02 +0100

    ScrolltoTop bug

---
+++ client/app/core/core.run.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    var core = angular.module('app.core');
+
+    core.run(runBlock);
+
+    runBlock.$inject = ['$rootScope'];
+
+
+    function runBlock($rootScope) {
+
+        $rootScope.$on('$viewContentLoaded',function(){
+            jQuery('html, body').animate({ scrollTop: 0 }, 200);
+        });    }
+
+})();
