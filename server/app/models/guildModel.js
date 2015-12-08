477ca393d4393ca54905275453ad57e313461f21
Baptiste <baptiste.routhieau@gmail.com>
2015-12-08 17:50:25 +0100

    Bug progress

--- server/app/models/guildModel.js
+++ server/app/models/guildModel.js
@@ -183,7 +183,6 @@ module.exports.computeProgress = function(region,realm,name,raid,callback){
                     reduced[values[idx].boss].timestamps = [];
                 }
 
-                foreach
                 reduced[values[idx].boss].timestamps.push(values[idx].timestamp);
             }
         }
