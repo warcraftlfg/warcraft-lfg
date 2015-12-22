c39f3d0fd99feda1c4956f249b3d40814b3fbe06
Bmagic <baptiste.routhieau@gmail.com>
2015-12-22 17:26:16 +0100

    Debug trace on socket

--- server/app/sockets/globalSocket.js
+++ server/app/sockets/globalSocket.js
@@ -6,11 +6,19 @@ var realmService = process.require("services/realmService.js");
 var logger = process.require("api/logger.js").get("logger");
 var applicationStorage = process.require("api/applicationStorage.js");
 
+
+//Configuration
+var env = process.env.NODE_ENV || 'dev';
+var config = process.require('config/config.'+env+'.json');
+var logger = process.require("api/logger.js").get("logger");
+
 module.exports.connect = function(){
     var io = applicationStorage.getSocketIo();
 
     io.on('connection', function(socket) {
         socket.on('get:search', function(search) {
+            logger.debug('get:search',socket.request.user);
+
             searchService.searchGuildsAndCharacters(search,function(error,result){
                 if (error)
                     return socket.emit("global:error", error.message);
@@ -19,6 +27,8 @@ module.exports.connect = function(){
         });
 
         socket.on('get:realms', function(realmZones) {
+            logger.debug('get:realms',socket.request.user);
+
             realmService.getFromRealmZones(realmZones,function(error,result){
                 if (error)
                     return socket.emit("global:error", error.message);
