2172e334f12e89dd12a155eea1dd13a5e4759394
baptiste <baptiste@bmagic.fr>
2016-01-03 17:51:31 +0100

    realm + faction criteria

--- server/app/guilds/guildRouter.js
+++ server/app/guilds/guildRouter.js
@@ -19,43 +19,52 @@ function getGuilds(req,res) {
     logger.verbose("%s %s %s", req.method, req.path, JSON.stringify(req.query));
 
     var criteria = {};
-    lfgCriteria.add(req.query,criteria);
-    factionCriteria.add(req.query,criteria);
-    realmCriteria.add(req.query,criteria);
-
-    var projection = {region:1,realm:1,name:1};
-    guildViewProjection.add(req.query,projection);
-
 
     var sort = {'ad.updated':-1};
     var limit = numberLimit.get(req.query);
+    var projection = {region:1,realm:1,name:1};
 
-    async.parallel({
-        guilds: function(callback){
-            if(limit > 0){
-                guildService.find(criteria,projection,sort,limit,function (error, guilds) {
-                    callback(error,guilds);
-                });
-            }
-            else{
-                callback(null,[]);
-            }
+    async.series([
+        function(callback){
+            //Call async criteria
+            factionCriteria.add(req.query,criteria);
+            lfgCriteria.add(req.query,criteria);
+            guildViewProjection.add(req.query,projection);
+            callback();
         },
-        count : function(callback){
-            guildService.count(criteria,function (error, count) {
-                callback(error,count);
-
+        function(callback){
+            //call async realmCriteria
+            realmCriteria.add(req.query,criteria,function(){
+                callback();
             });
         }
-    },function(error,results){
-        if(error){
-            logger.error(error.message);
-            res.status(500).send();
-        }
-        res.setHeader('X-Total-Count',results.count);
-        res.json(results.guilds);
-    });
+    ],function(){
+        async.parallel({
+            guilds: function(callback){
+                if(limit > 0){
+                    guildService.find(criteria,projection,sort,limit,function (error, guilds) {
+                        callback(error,guilds);
+                    });
+                }
+                else{
+                    callback(null,[]);
+                }
+            },
+            count : function(callback){
+                guildService.count(criteria,function (error, count) {
+                    callback(error,count);
 
+                });
+            }
+        },function(error,results){
+            if(error){
+                logger.error(error.message);
+                res.status(500).send();
+            }
+            res.setHeader('X-Total-Count',results.count);
+            res.json(results.guilds);
+        });
+    });
 }
 
 //Define routes
--- server/app/params/criteria/realmCriteria.js
+++ server/app/params/criteria/realmCriteria.js
@@ -1,28 +1,63 @@
 "use strict";
 
+var async = require("async");
+var realmZonesCriteria = process.require("params/criteria/realmZonesCriteria.js");
+var realmService = process.require("realms/realmService.js");
+var applicationStorage = process.require("api/applicationStorage.js");
+
 /**
  * Add the realm criteria from realm param or from realmZonesParams
  * @param query
  * @param criteria
  */
-module.exports.add = function(query,criteria){
+module.exports.add = function(query,criteria,callback){
 
-    //TODO if realmZoneCriteria is set and realm not set send realms into criteria
-    var realms = [];
-    if(query.realm!=undefined && query.realm !== "") {
-        try {
-            var realm = JSON.parse(query.realm);
-            if(realm.region!=undefined && realm.name !=undefined)
-                realms=[{region:realm.region,realm:realm.name}];
-        }
-        catch (e){
-        }
-    }
+    var logger = applicationStorage.logger;
 
-    if (realms.length > 0) {
-        if(!criteria["$or"])
-            criteria["$or"] = realms;
-        else
-            criteria["$or"] = criteria["$or"].concat(realms);
-    }
+    var realmList= [];
+
+    async.series([
+        function(callback){
+            var realmCriteria = {};
+            realmZonesCriteria.add(query,realmCriteria);
+            if(realmCriteria['$or']!=undefined){
+                realmService.find(realmCriteria,function(error,realms){
+                    if (error)
+                        logger.error(error.message);
+                    else {
+                        async.forEach(realms,function(realm,callback){
+                            realmList.push({region:realm.region,realm:realm.name});
+                            callback();
+
+                        },function(){
+                            callback();
+                        })
+
+                    }
+                });
+            }
+        },
+        function(callback){
+            if(query.realm!=undefined && query.realm !== "") {
+                try {
+                    var realm = JSON.parse(query.realm);
+                    if(realm.region!=undefined && realm.name !=undefined)
+                        realmList=[{region:realm.region,realm:realm.name}];
+                }
+                catch (e){
+                }
+                callback();
+            }
+
+        }
+    ],function(){
+
+        if (realmList.length > 0) {
+            if(!criteria["$or"])
+                criteria["$or"] = realmList;
+            else
+                criteria["$or"] = criteria["$or"].concat(realmList);
+        }
+        callback();
+    });
 };
