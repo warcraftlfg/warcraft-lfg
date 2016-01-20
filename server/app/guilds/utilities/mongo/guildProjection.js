5b4f1596315220f0a8a233b453d95d4a47ace4f7
Baptiste <baptiste.routhieau@gmail.com>
2016-01-20 15:44:00 +0100

    pveScore bug

--- server/app/guilds/utilities/mongo/guildProjection.js
+++ server/app/guilds/utilities/mongo/guildProjection.js
@@ -23,7 +23,9 @@ module.exports.get = function(query){
             projection["progress."+raid.name+".normalCount"] = 1;
             projection["progress."+raid.name+".heroicCount"] = 1;
             projection["progress."+raid.name+".mythicCount"] = 1;
+            projection["progress."+raid.name+".pveScore"] = 1;
         });
+
 
     }
     if (query.view === "minimal") {
