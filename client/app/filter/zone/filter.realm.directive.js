eabd5ccd3caf548a2a8b42bb37e82de97982188b
baptiste <baptiste@bmagic.fr>
2016-02-06 23:35:16 +0100

    End of character list

--- client/app/filter/zone/filter.realm.directive.js
+++ client/app/filter/zone/filter.realm.directive.js
@@ -2,8 +2,8 @@ angular
     .module('app.filter')
     .directive('wlfgFilterRealm', wlfgFilterRealm);
 
-wlfgFilterRealm.$inject = ['$translate', '$stateParams', '$location','realms'];
-function wlfgFilterRealm($translate, $stateParams, $location,realms) {
+wlfgFilterRealm.$inject = ['$translate', '$stateParams', '$location', 'realms'];
+function wlfgFilterRealm($translate, $stateParams, $location, realms) {
     var directive = {
         link: link,
         restrict: 'A',
@@ -16,11 +16,11 @@ function wlfgFilterRealm($translate, $stateParams, $location,realms) {
         $scope.realms = [];
 
         $scope.localRealms = {
-            selectAll       : $translate.instant("SELECT_ALL"),
-            selectNone      : $translate.instant("SELECT_NONE"),
-            reset           : $translate.instant("RESET"),
-            search          : $translate.instant("SEARCH"),
-            nothingSelected : $translate.instant("ALL_REALMS")
+            selectAll: $translate.instant("SELECT_ALL"),
+            selectNone: $translate.instant("SELECT_NONE"),
+            reset: $translate.instant("RESET"),
+            search: $translate.instant("SEARCH"),
+            nothingSelected: $translate.instant("ALL_REALMS")
         };
 
         $scope.filters.realm = null;
@@ -30,7 +30,7 @@ function wlfgFilterRealm($translate, $stateParams, $location,realms) {
 
             var params = realm.split('.');
 
-            if(params.length == 2) {
+            if (params.length == 2) {
                 $scope.realms = [{
                     label: params[1] + " (" + params[0].toUpperCase() + ")",
                     selected: true
@@ -41,55 +41,47 @@ function wlfgFilterRealm($translate, $stateParams, $location,realms) {
 
         $scope.filters.states.realm = true;
 
-        $scope.$watch('realmOut',function(){
+        $scope.$watch('realmOut', function () {
 
             if ($scope.$parent.loading || $scope.loading) {
                 return;
             }
 
-            if($scope.realmOut){
-                $location.search('realm', $scope.realmOut.region+"."+$scope.realmOut.name);
-                $scope.filters.realm =  $scope.realmOut.region+"."+$scope.realmOut.name;
-            }else
-            {
+            if ($scope.realmOut) {
+                $location.search('realm', $scope.realmOut.region + "." + $scope.realmOut.name);
+                $scope.filters.realm = $scope.realmOut.region + "." + $scope.realmOut.name;
+            } else {
                 $location.search('realm', null);
-                $scope.filters.realm=null;
+                $scope.filters.realm = null;
             }
 
             $scope.$parent.loading = true;
-        },true);
+        }, true);
 
-        $scope.setRealm = function(data){
+        $scope.setRealm = function (data) {
             $scope.realmOut = data;
         };
 
-        $scope.resetRealm = function(){
+        $scope.resetRealm = function () {
             $scope.realmOut = null;
-            angular.forEach($scope.realms,function(realm) {
+            angular.forEach($scope.realms, function (realm) {
                 realm.selected = false;
             });
         };
 
-        $scope.$on('get:realms', function() {
-            realms.query({realm_zone:$scope.filters.realm_zone},function(realms){
+        $scope.$on('get:realms', function () {
+            realms.query({realm_zone: $scope.filters.realm_zone}, function (realms) {
                 $scope.realms = realms;
-                angular.forEach(realms,function (realm) {
+                angular.forEach(realms, function (realm) {
                     realm.label = realm.name + " (" + realm.region.toUpperCase() + ")";
-
-                    if($stateParams.realm) {
+                    if ($stateParams.realm) {
                         var params = $stateParams.realm.split('.');
                         if (params.length == 2 && params[1] == realm.name && params[0] == realm.region) {
                             realm.selected = true;
                         }
                     }
-
-
                 });
             });
-
-
-
         });
-
     }
 }
