cba732fc64f62b19e8b379fabe65e57ff1f2ccde
Bmagic <baptiste.routhieau@gmail.com>
2016-02-07 23:59:34 +0100

    DayCriterion bug.

--- server/app/characters/utilities/mongo/criteria/dayCriterion.js
+++ server/app/characters/utilities/mongo/criteria/dayCriterion.js
@@ -29,11 +29,12 @@ module.exports.add = function (query, criteria) {
                 if (start != 0 || end != 0) {
                     var offset = Math.round(zone.parse(Date.UTC()) / 60);
                     if(end>start){
-                        tmpObj["ad.play_time." + day + ".start.hourUTC"] = {"$lte": start + offset};
-                        tmpObj["ad.play_time." + day + ".end.hourUTC"] = {"$gte": end + offset};
-                    } else {
                         tmpObj["ad.play_time." + day + ".start.hourUTC"] = {"$gte": start + offset};
                         tmpObj["ad.play_time." + day + ".end.hourUTC"] = {"$lte": end + offset};
+                    } else {
+                        tmpObj["ad.play_time." + day + ".start.hourUTC"] = {"$lte": start + offset};
+                        tmpObj["ad.play_time." + day + ".end.hourUTC"] = {"$gte": end + offset};
+
                     }
                 }
                 days.push(tmpObj);
