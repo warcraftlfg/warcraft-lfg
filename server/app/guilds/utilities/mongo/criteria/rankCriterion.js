8786f563382bfe67a93a4948f417f8b62d27c04f
baptiste <baptiste@bmagic.fr>
2016-03-06 11:05:03 +0100

    Progress rank criterion change for best performance.

--- server/app/guilds/utilities/mongo/criteria/rankCriterion.js
+++ server/app/guilds/utilities/mongo/criteria/rankCriterion.js
@@ -13,7 +13,7 @@ module.exports.add = function (query, criteria) {
     var paramArray = params.parseQueryParam(query.rank, 1);
     if (paramArray.length > 0) {
         if (paramArray[0][0] === 'true') {
-            criteria['rank'] = {"$exists":true};
+            criteria['rank.Hellfire Citadel.world'] = {"$exists":true};
         }
     }
 };
