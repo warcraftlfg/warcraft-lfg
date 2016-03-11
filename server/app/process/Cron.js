b0270aa5d49102d3c8cdeb4e67f9df6f4503b213
baptiste <baptiste@bmagic.fr>
2016-03-11 23:31:50 +0100

    Simplify everything (Remove progress)

--- server/app/process/Cron.js
+++ server/app/process/Cron.js
@@ -76,7 +76,7 @@ Cron.prototype.cleanAds = function () {
                 });
             },
             function (callback) {
-                //Set to Update Character Ads
+                //Set to Update Guild Ads
                 async.waterfall([
                     function (callback) {
                         guildModel.find({"ad.lfg": true}, {region: 1, realm: 1, name: 1}, function (error, guilds) {
@@ -85,7 +85,7 @@ Cron.prototype.cleanAds = function () {
                     },
                     function (guilds, callback) {
                         async.each(guilds, function (guild, callback) {
-                            updateModel.insert('cu', guild.region, guild.realm, guild.name, 3, function (error) {
+                            updateModel.insert('gu', guild.region, guild.realm, guild.name, 3, function (error) {
                                 logger.verbose("Insert guild to update %s-%s-%s to update with priority 3", guild.region, guild.realm, guild.name);
                                 callback(error);
                             });
