3294e3af7d36c5b9378d5a761991cbb52df42285
baptiste <baptiste@bmagic.fr>
2016-03-23 21:54:48 +0100

    Fixed missing language Bug.

--- client/app/core/core.js
+++ client/app/core/core.js
@@ -1,22 +1,24 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('app.core')
         .controller('CoreController', Core);
 
-    Core.$inject = ['$scope','$translate','socket','wlfgAppTitle','user'];
+    Core.$inject = ['$scope', '$translate', 'socket', 'wlfgAppTitle', 'user'];
 
-    function Core($scope,$translate,socket,wlfgAppTitle,user) {
+    function Core($scope, $translate, socket, wlfgAppTitle, user) {
         $scope.wlfgAppTitle = wlfgAppTitle;
 
-        $scope.currentLanguage = $translate.use();
-        $scope.setLanguage = function (key){
+        $scope.currentLanguage = $translate.use() ||
+            $translate.preferredLanguage();
+
+        $scope.setLanguage = function (key) {
             $translate.use(key);
             $scope.currentLanguage = $translate.use();
         };
 
-        $scope.user = user.get({param:"profile"});
+        $scope.user = user.get({param: "profile"});
 
     }
 })();
