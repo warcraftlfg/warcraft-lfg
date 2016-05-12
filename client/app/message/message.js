77f6cb251ff4a18a2a5e3fb1237bf51204c45721
Alogon <ae.alogon@gmail.com>
2016-05-12 22:25:35 +0200

    Message UI v0.2

--- client/app/message/message.js
+++ client/app/message/message.js
@@ -5,6 +5,7 @@
         .module('app.message')
         .controller('MessageController', Message)
         .filter('conversationOrderFilter', ConversationOrderFilter)
+        .filter('conversationDateFilter', ConversationDateFilter)
     ;
 
     Message.$inject = ["$scope", "socket", "$stateParams", "messages"];
@@ -23,7 +24,6 @@
                 $scope.$parent.loading = false;
                 $scope.messages = result.messages;
                 $scope.entities = result.entities;
-                console.log($scope.messages);
                 angular.forEach(result.entities, function(value, key) {
                     if (!Array.isArray(value.id)) {
                         $scope.entitiesKey[value.id] = value;
@@ -31,7 +31,6 @@
 
                     }
                 });
-                console.log($scope.entitiesKey);
             }, function (error) {
                 $scope.$parent.error = error.data;
                 $scope.$parent.loading = false;
@@ -70,12 +69,48 @@
         $scope.$on('socket:newMessage', function (ev, message) {
             if ((message.objIds[0] == $stateParams.objId1 || message.objIds[0] == $stateParams.objId2) && (message.objIds[1] == $stateParams.objId1 || message.objIds[1] == $stateParams.objId2)) {
                 $scope.messages.push(message);
-                console.log('push message');
                 $(".messages").getNiceScroll().resize();
                 $scope.reInit();
             }
             getConversations();
         });
+
+        $scope.getStyle = function(index) {
+            if (index == 0) {
+                $scope.messages[index].style = "#1b2737";
+                $scope.messages[index].styleInverse = "#1d222c";
+                return { 
+                    'background-color': '#1b2737'
+                };
+            } else if ($scope.messages[index-1] && $scope.messages[index-1].userId ==  $scope.messages[index].userId)  {
+                $scope.messages[index].style = $scope.messages[index-1].style;
+                $scope.messages[index].styleInverse = $scope.messages[index-1].styleInverse;
+                return { 
+                    'background-color': $scope.messages[index-1].style
+                };         
+            } else if ($scope.messages[index-1] && $scope.messages[index-1].userId !=  $scope.messages[index].userId)  {
+                $scope.messages[index].style = $scope.messages[index-1].styleInverse;
+                $scope.messages[index].styleInverse = $scope.messages[index-1].style;
+                return { 
+                    'background-color': $scope.messages[index-1].styleInverse
+                };   
+            }
+        }
+
+        $scope.getDisplay = function(index) {
+            if (index == 0) {
+                return true;
+            } else if ($scope.messages[index-1] && $scope.messages[index-1].userId ==  $scope.messages[index].userId) {
+                var timestamp1 = parseInt(("" + $scope.messages[index-1]._id).substr(0, 8), 16) * 1000;
+                var timestamp2 = parseInt(("" + $scope.messages[index]._id).substr(0, 8), 16) * 1000;
+                if ((timestamp2 - timestamp1) > 60000) {
+                    return true;
+                }
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 
     ConversationOrderFilter.$inject = [];
@@ -86,6 +121,20 @@
             } else {
                 return item;
             }
+        }
+    }
+
+    ConversationDateFilter.$inject = ['moment'];
+    function ConversationDateFilter(moment) {
+        return function(item) {
+            var date = new moment();
+            if (date.format('DD-MM-YYYY') == item.format('DD-MM-YYYY')) {
+                return item.format('HH:mm');
+            } else {
+                return item.format('DD/MM/YYYY HH:mm');
+            }
+            console.log(date);
+            console.log(item);
         }
     }
 
