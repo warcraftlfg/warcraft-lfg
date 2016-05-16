206b0a6abf1adb5533960e25ac54338ab4540121
baptiste <baptiste@bmagic.fr>
2016-05-16 11:47:22 +0200

    Add moment translation

--- client/app/core/core.js
+++ client/app/core/core.js
@@ -5,17 +5,20 @@
         .module('app.core')
         .controller('CoreController', Core);
 
-    Core.$inject = ['$scope', '$translate', 'socket', 'wlfgAppTitle', 'user'];
+    Core.$inject = ['$scope', '$translate', 'socket', 'wlfgAppTitle', 'user', 'amMoment'];
 
-    function Core($scope, $translate, socket, wlfgAppTitle, user) {
+    function Core($scope, $translate, socket, wlfgAppTitle, user, amMoment) {
         $scope.wlfgAppTitle = wlfgAppTitle;
 
         $scope.currentLanguage = $translate.use() ||
             $translate.preferredLanguage();
 
+        amMoment.changeLocale($translate.use() || $translate.preferredLanguage());
+
         $scope.setLanguage = function (key) {
             $translate.use(key);
             $scope.currentLanguage = $translate.use();
+            amMoment.changeLocale(key);
         };
 
         $scope.user = user.get({param: "profile"});
