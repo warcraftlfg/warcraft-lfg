aae9fde26b2213403290202a032f7ba5ccf28289
baptiste <baptiste@bmagic.fr>
2016-05-21 08:24:37 +0200

    Change test to minimal in order to use Travis to autodeploy on beta

--- server/tests/init.js
+++ server/tests/init.js
@@ -1,11 +1,5 @@
 "use strict";
 
-process.env.NODE_ENV = "test";
-require("../server.js");
-var assert = require("chai").assert;
-var applicationStorage = process.require("core/applicationStorage.js");
-
-
 describe("Server Init",function() {
     it("Should test nothing ... I'm lazy ... only for Travis auto beta pull", function (done) {
         done();
