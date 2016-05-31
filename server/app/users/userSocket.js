d97c267c5d7134e1423a4c9a8c2615579c036fcd
Baptiste <baptiste.routhieau@gmail.com>
2016-05-31 15:43:16 +0200

    Socket IP log correct IP.

--- server/app/users/userSocket.js
+++ server/app/users/userSocket.js
@@ -7,23 +7,23 @@ module.exports.connect = function () {
     var logger = applicationStorage.logger;
     applicationStorage.socketIo.on('connection', function (socket) {
         if (socket.request.user.logged_in == true) {
-            logger.info("%s User %s connected", socket.conn.remoteAddress || socket.handshake.headers['x-forwarded-for'], socket.request.user.battleTag);
+            logger.info("%s User %s connected", socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress, socket.request.user.battleTag);
             if (applicationStorage.users[socket.request.user.id]) {
                 applicationStorage.users[socket.request.user.id].push(socket.id);
             } else {
                 applicationStorage.users[socket.request.user.id] = [socket.id];
             }
         } else {
-            logger.info("%s User anonymous connected", socket.conn.remoteAddress || socket.handshake.headers['x-forwarded-for']);
+            logger.info("%s User anonymous connected", socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress);
         }
         socket.on('disconnect', function () {
             if (socket.request.user.logged_in == true) {
-                logger.info("%s User %s disconnected",  socket.conn.remoteAddress || socket.handshake.headers['x-forwarded-for'],socket.request.user.battleTag);
+                logger.info("%s User %s disconnected",  socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress,socket.request.user.battleTag);
                 lodash.remove(applicationStorage.users[socket.request.user.id], function (socketId) {
                     return socketId == socket.id
                 });
             } else {
-                logger.info("%s User anonymous disconnected", socket.conn.remoteAddress || socket.handshake.headers['x-forwarded-for']);
+                logger.info("%s User anonymous disconnected",socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress);
             }
         });
 
