1d35afa14d3c5dc0d872b101d067bee2d00b0cd3
Baptiste <baptiste.routhieau@gmail.com>
2016-06-08 17:21:42 +0200

    Fix character progress bug

---
+++ server/app/characters/utilities/mongo/criteria/progressCriterion.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//Load dependencies
+var params = process.require("core/utilities/params.js");
+var applicationStorage = process.require("core/applicationStorage.js");
+
+/**
+ * Add the progression criterion in criteria
+ * @param query
+ * @param criteria
+ */
+module.exports.add = function (query, criteria) {
+
+    var paramArray = params.parseQueryParam(query.progress, 3);
+    if (paramArray.length > 0) {
+        var min = parseInt(paramArray[0][1], 10);
+        var max  = parseInt(paramArray[0][2], 10);
+        if (isNaN(min) || isNaN(max)) {
+            return;
+        }
+        var progressFactor;
+        if (paramArray[0][0] == "normal") {
+            progressFactor = 1000;
+        } else if (paramArray[0][0] == "heroic") {
+            progressFactor = 100000;
+        } else if (paramArray[0][0] == "mythic") {
+            progressFactor = 10000000;
+        } else {
+            return;
+        }
+
+        criteria["progress.score"] = {$lte: (max+1) * progressFactor,$gte: min * progressFactor};
+    }
+};
