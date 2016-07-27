d3808255f8e90f8bae4846a74382a2154e4afd5b
Bmagic <baptiste.routhieau@gmail.com>
2016-07-28 00:46:11 +0200

    Modification to fit with api progress

--- client/app/progress/progress.js
+++ client/app/progress/progress.js
@@ -8,28 +8,39 @@
     Progress.$inject = ["$scope", "$stateParams", "$location", "wlfgAppTitle", "ranking"];
     function Progress($scope, $stateParams, $location, wlfgAppTitle, ranking) {
         wlfgAppTitle.setTitle("WarcraftProgress");
-        $scope.filters = {};
-
+        $scope.filters = {tier:'18',realm_zones:""};
+        $scope.filters.states = {};
         //Reset error message
         $scope.$parent.error = null;
-        $scope.path =  "pve/";
+        $scope.path = "pve/";
         if ($stateParams.region) {
-            $scope.path += $stateParams.region+"/";
+            $scope.path += $stateParams.region + "/";
             $scope.filters.region = $stateParams.region;
         }
         if ($stateParams.realm) {
-            $scope.path += $stateParams.realm+"/";
+            $scope.path += $stateParams.realm + "/";
             $scope.filters.realm = $stateParams.realm;
         }
 
-        $scope.page = ($stateParams.page > 0) ? parseInt($stateParams.page) : 1;
-        
-        ranking.get({limit:19, start: ($scope.page-1)*20}, function(ranking) {
-            if (ranking) {
-                $scope.ranking = ranking;
-            } else {
-                $scope.noResult = true;
-            }
+
+        $scope.$watch('filters', function () {
+                getRankings();
         });
+
+        $scope.page = ($stateParams.page > 0) ? parseInt($stateParams.page) : 1;
+
+
+        function getRankings() {
+            ranking.get({tier: $scope.filters.tier, limit: 19, start: ($scope.page - 1) * 20}, function (ranking) {
+                if (ranking) {
+                    $scope.ranking = ranking;
+                    $scope.$parent.loading = false;
+                } else {
+                    $scope.noResult = true;
+                    $scope.$parent.loading = false;
+
+                }
+            });
+        }
     }
 })();
