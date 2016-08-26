e0511e2b3a86a3bf3a156baf35bea98b6b40c720
Baptiste <baptiste.routhieau@gmail.com>
2016-08-26 10:01:05 +0200

    Parse guild when parser is active

--- server/app/guilds/guildController.js
+++ server/app/guilds/guildController.js
@@ -184,7 +184,23 @@ module.exports.putGuildParser = function (req, res) {
 
     var parser = req.body;
     parser.updated = new Date().getTime();
-    guildModel.upsert(req.params.region, req.params.realm, req.params.name, {parser: parser}, function (error) {
+
+    async.series([
+        function(callback){
+            if(parser.active == true){
+                updateModel.insert('gu', req.params.region, req.params.realm, req.params.name, 5, function (error) {
+                    callback(error);
+                });
+            }else{
+                callback();
+            }
+        },
+        function(callback){
+            guildModel.upsert(req.params.region, req.params.realm, req.params.name, {parser: parser}, function (error) {
+                callback(error);
+            });
+        }
+    ],function(error){
         if (error) {
             logger.error(error.message);
             res.status(500).send(error.message);
@@ -192,6 +208,7 @@ module.exports.putGuildParser = function (req, res) {
             res.json();
         }
     });
+
 
 };
 
