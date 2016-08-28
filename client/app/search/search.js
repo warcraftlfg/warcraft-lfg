8f04e43fbea8520702f5b1637d8a7e02fcdfbbef
Alogon <ae.alogon@gmail.com>
2016-08-28 22:26:25 +0200

    Loading & no data for search

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -12,13 +12,20 @@
 
         $scope.term = $stateParams.term;
 
+        $scope.loading = true;
+
         search.get({
             "search": $scope.term
         }, function(guilds) {
             $scope.limit = Math.ceil(guilds.length / 3);
             $scope.guilds = guilds;
+            $scope.loading = false;
+            if (guilds.length <= 0) {
+                $scope.noResult = true;
+            }
         }, function(error) {
-
+            $scope.loading = false;
+            $scope.noResult = true;
         });
 
     }
