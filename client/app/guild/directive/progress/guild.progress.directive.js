a75e987f6d8b48c924a507b7472a6e16903cd654
Alogon <ae.alogon@gmail.com>
2016-11-08 21:48:56 +0100

    Fix progress tooltip

--- client/app/guild/directive/progress/guild.progress.directive.js
+++ client/app/guild/directive/progress/guild.progress.directive.js
@@ -41,6 +41,7 @@ function wlfgProgressGuild(__env, progress) {
                         scope.progressTooltip.push({difficulty: 'common', 'boss': 'N: '+value});
                     });
                 } else {
+                    scope.progressTooltipLoad = true;
                     buildTooltip();
                 }
             } else {
@@ -68,11 +69,11 @@ function wlfgProgressGuild(__env, progress) {
             var progress = scope.progress;
             if (progress) {
                 angular.forEach(__env.tiers[__env.tiers.current[raidKey]].bosses, function(value, key) {
-                    if (progress.mythic && progress.mythic[value] && progress.mythic[value].timestamps && progress.mythic[value].timestamps.length > 0) {
+                    if (progress.mythic && progress.mythic[value] && ((progress.mythic[value].timestamps && progress.mythic[value].timestamps.length > 0) || progress.mythic[value] > 0)) {
                         scope.progressTooltip.push({difficulty: 'legendary', 'boss': 'M: '+value});
-                    } else if (progress.heroic && progress.heroic[value] && progress.heroic[value].timestamps && progress.heroic[value].timestamps.length > 0) {
+                    } else if (progress.heroic && progress.heroic[value] && ((progress.heroic[value].timestamps && progress.heroic[value].timestamps.length > 0) || progress.heroic[value] > 0)) {
                         scope.progressTooltip.push({difficulty: 'epic', 'boss': 'H: '+value});
-                    } else if (progress.normal && progress.normal[value] && progress.normal[value].timestamps && progress.normal[value].timestamps.length > 0) {
+                    } else if (progress.normal && progress.normal[value] && ((progress.normal[value].timestamps && progress.normal[value].timestamps.length > 0) || progress.normal[value] > 0)) {
                         scope.progressTooltip.push({difficulty: 'rare', 'boss': 'N: '+value});
                     } else {
                          scope.progressTooltip.push({difficulty: 'common', 'boss': 'N: '+value});
